b23e535f604b6552eb81d6d3eafa288481679c81
Michal Juhas <michal.juhas@hotelquickly.com>
2016-05-19 14:13:15 +0700

    Improved   example.sql

--- example.sql
+++ example.sql
@@ -2,11 +2,15 @@
 
 
 SELECT /* ex 01 */
-	id AS employee_id,
+	employee_id AS employee_id,
 	first_name,
-	last_name
+	last_name,
+ 	contract.sign_date
 FROM employee
+INNER JOIN contract ON 1=1
+	and contract.employee_id = employee.id
+	and contract.deleted_flag = 0
 WHERE 1=1
-	AND deleted_flag = 0
+	AND employee.deleted_flag = 0
 LIMIT 100
 ;
